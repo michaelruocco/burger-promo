0d1bef628d03286439050595a1282389c5950bf9
Michael Ruocco <michael.ruocco@hotmail.com>
2021-06-07 19:27:59 +0100

    applies spotless

--- src/main/java/uk/co/mruoc/promo/controller/AccountController.java
+++ src/main/java/uk/co/mruoc/promo/controller/AccountController.java
@@ -5,13 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.co.mruoc.promo.entity.account.Account;
 import uk.co.mruoc.promo.usecase.account.AccountService;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
